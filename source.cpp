6473445b880920a6206d9f230fb4902e5b5af399
scammerlikely <137663707+scammerlikely@users.noreply.github.com>
2023-11-07 19:53:54 -0500

    Create source.cpp

---
+++ source.cpp
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+ // using Webhook;
+/// <summary>
+///  #RR dm to buy ( dumper_id_profile ) 
+/// </summary>
+namespace dd
+{
+    static class Program
+    {
+        private static Form placehandle;
+        private static Form webhook; // ignore 
+        private static Form static3; 
+        private static object corner;
+
+        private static object si23m;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// 
+    /// #rr 
+// pp open source ayoo
+// please run please!! ?!?!?
+// discord
+
+        // i like dudes and they nut in my ooddssss 
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Skin());
+           Application.Run(webhook("your url here ")); // url -_- 
+             Application.Run(static3(si23m));
+            Application.Run(placehandle(corner));
+        }
+    private static Form static3(object si23m)
+        {
+            throw new NotImplementedException();
+        }
+        private static Form placehandle(object corner)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
